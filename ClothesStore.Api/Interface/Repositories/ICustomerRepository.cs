9f68b0be7c027f3cd58e9fd715d24050339ebf37
Danh <truongcongdanh2736@gmail.com>
2026-09-15 16:41:46 +0700

    [Add] them dto va repository customer

---
+++ ClothesStore.Api/Interface/Repositories/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using ClothesStore.Api.Models;
+
+namespace ClothesStore.Api.Interface.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetAllAsync();
+        Task<Customer?> GetByIdAsync(int id);
+        Task<Customer?> GetByIdWithDetailsAsync(int id); // kèm MembershipTier, Addresses
+        Task<bool> ApplicationUserExistsAsync(string applicationUserId);
+        Task<bool> MembershipTierExistsAsync(int tierId);
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(Customer customer);
+    }
+}
---
+++ ClothesStore.Api/Repositories/CustomerRepository.cs
@@ -0,0 +1,53 @@
+﻿using ClothesStore.Api.Data;
+using ClothesStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothesStore.Api.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomerRepository(ApplicationDbContext context) => _context = context;
+
+        public async Task<List<Customer>> GetAllAsync()
+            => await _context.Customers
+                .Include(c => c.MembershipTier)
+                .Include(c => c.Addresses)
+                .Include(c => c.Wishlists)
+                .ToListAsync();
+
+        public async Task<Customer?> GetByIdAsync(int id)
+            => await _context.Customers.FindAsync(id);
+
+        public async Task<Customer?> GetByIdWithDetailsAsync(int id)
+            => await _context.Customers
+                .Include(c => c.MembershipTier)
+                .Include(c => c.Addresses)
+                .Include(c => c.Wishlists)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+        public async Task<bool> ApplicationUserExistsAsync(string applicationUserId)
+            => await _context.Users.AnyAsync(u => u.Id == applicationUserId);
+
+        public async Task<bool> MembershipTierExistsAsync(int tierId)
+            => await _context.MembershipTiers.AnyAsync(t => t.Id == tierId);
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
