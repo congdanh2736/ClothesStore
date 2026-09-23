cbfc5a794a51f0a57d8cfdcc07a5c039da49c84f
Danh <truongcongdanh2736@gmail.com>
2026-09-23 09:41:52 +0700

    [add] login dto

---
+++ ClothesStore.Api/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ClothesStore.Api.DTOs.Auth
+{
+    // Lớp gửi request đăng nhập từ client đến server, chứa thông tin email và password của người dùng.
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
