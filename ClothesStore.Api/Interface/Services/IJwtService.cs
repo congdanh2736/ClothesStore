272a4c642fdc85b3b8ea3d97376377d6d1868167
Danh <truongcongdanh2736@gmail.com>
2026-09-23 09:35:19 +0700

    [Add] Them JwtService

---
+++ ClothesStore.Api/Interface/Services/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using ClothesStore.Api.Models;
+
+namespace ClothesStore.Api.Interface.Services
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateTokenAsync(ApplicationUser user);
+    }
+}
